15a7c8d1cde59e7dd3884671a1e3dc30b51c095c
Travis-DV <findleytm2018@gmail.com>
2026-04-09 00:51:17 -0500

    Last try (I hope)

---
+++ WGU_C969-Software-II-CS/CustomerForm.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WGU_C969_Software_II_CS;
+
+public partial class CustomerForm : Window
+{
+    public CustomerForm()
+    {
+        InitializeComponent();
+    }
+}
